6a56161f63b8c3e8e3e6b1bb8945957702776ead
Will McElhenney <wmcelhe1@jhu.edu>
2020-06-30 00:38:20 -0500

    Working and done

--- dragonballscraper.py
+++ dragonballscraper.py
@@ -16,13 +16,12 @@ soup = [s.find_all('a', href=True) for s in soup]
 volume_links = [vl['href'] for s in soup for vl in s]
 volume_links = [base_url + vl for vl in volume_links]
 
+re_vol_num = re.compile(r'did=\w-\w-([0-9]+)')
+re_page_num = re.compile(r'DRAGONBALLvol[\w]+/([0-9\-]+)-[\w]+\.jpg')
 
 def scrape_images(volume_links, http_fails=None):
 	if http_fails is None:
 		http_fails = []
-		
-	re_vol_num = re.compile(r'did=\w-\w-([0-9]+)')
-	re_page_num = re.compile(r'DRAGONBALLvol[\w]+/([0-9\-]+)-[\w]+\.jpg')
 
 	dir_path = 'data/{}'
 	
@@ -48,12 +47,12 @@ def scrape_images(volume_links, http_fails=None):
 	
 		for pl in page_links:
 			# get the image
-			page = requests.get(pl, stream=True)				
-			
-			# get page number
 			try:
+				page = requests.get(pl, stream=True)	
+				# get page number
 				page_num = re_page_num.search(pl).group(1)
 			
+				
 				if page.status_code == 200:
 					print(f"Writing {vl}, {page_num}")
 			
@@ -67,10 +66,40 @@ def scrape_images(volume_links, http_fails=None):
 				http_fails.append((vl, pl))
 							
 	return http_fails
+
+def clear_fails(http_fails, dir_path = 'data/{}'):
+	new_http_fails = []
+	
+	for vl, pl in http_fails:
+		vol_num = re_vol_num.search(vl).group(1)
+		page_num = re_page_num.search(pl).group(1)
+		path = dir_path.format(vol_num)
 		
+		try:
+			page = requests.get(pl, stream=True)
+	
+			if page.status_code == 200:
+				print(f"Writing {vl}, {page_num}")
+		
+				img_path = path + '/' + page_num + '.jpg'
+				with open(img_path, 'wb') as file:
+					page.raw.decode_content = True
+					shutil.copyfileobj(page.raw, file)
+					
+		except:
+			print(f"FAILED: {vl}, {pl}")
+			new_http_fails.append((vl, pl))
+			
+	if len(new_http_fails) > 0:
+		clear_fails(new_http_fails)
+	
 	
 http_fails = scrape_images(volume_links)
 
-with open('fails.txt', 'w') as f:
-	for fail in http_fails:
-		f.write(f'({fail[0]}, {fail[1]})\n')
+#with open('fails.txt', 'w') as f:
+#	for fail in http_fails:
+#		vl, pl = *fail
+#		f.write(f'({vl}, {pl})\n')
+		
+clear_fails(http_fails)
+		
